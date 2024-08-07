173f030b7102ea7458e020823cd112fb4e16a1cb
hbbo0e <44goodluck44@gmail.com>
2024-08-07 22:52:06 +0900

    환경 설정 완료

---
+++ boardback/src/main/java/com/hbbo/boardback/BoardbackApplication.java
@@ -0,0 +1,13 @@
+package com.hbbo.boardback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardbackApplication.class, args);
+	}
+
+}
